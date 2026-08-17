5ebc8c5a64489c369b7b47d76f375c357fcea302
jefersonfx1 <139823856+jefersonfx1@users.noreply.github.com>
2026-08-17 14:11:42 -0300

    feat: estrutura inicial do projeto Next.js 15 + Tailwind + Netlify

--- next.config.ts
+++ next.config.ts
@@ -3,8 +3,14 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
-      { protocol: "https", hostname: "**.supabase.co" },
-      { protocol: "https", hostname: "images.unsplash.com" },
+      {
+        protocol: "https",
+        hostname: "**.supabase.co",
+      },
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
     ],
   },
 };
